b5d084174565a2528e3bd60978f6d4e783a9a005
Facundo Ortiz <86033840+FakuOrtiz@users.noreply.github.com>
2023-05-09 18:26:49 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer>
       <p>
         Developed by Facundo Ortiz
-        <a href="https://www.linkedin.com/in/facundo-agustin-ortiz-gomez/" target="_blank" className="fa-brands fa-linkedin linkedin"/>
+        <a href="https://www.linkedin.com/in/facundortiz/" target="_blank" className="fa-brands fa-linkedin linkedin"/>
         <a href="https://github.com/FakuOrtiz" target="_blank" className="fa-brands fa-github github" />
       </p>
       <p>Copyright © 2022</p>
